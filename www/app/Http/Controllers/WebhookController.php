31a4cb0c8a8bd6e9f82a5cff913d751c90aaa5e2
acferten <gowiththeirname@gmail.com>
2023-09-26 09:43:19 +0700

    initial

---
+++ www/app/Http/Controllers/WebhookController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Telegram\Bot\BotsManager;
+
+class WebhookController extends Controller
+{
+    protected BotsManager $botsManager;
+
+    public function __construct(BotsManager $botsManager)
+    {
+        $this->botsManager = $botsManager;
+    }
+
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request): Response
+    {
+        $this->botsManager->bot()->commandsHandler(true);
+        return response(null, 200);
+    }
+}
