b2bbd226849fd35edd158430ec9f41e7fcdf331d
acferten <gowiththeirname@gmail.com>
2023-09-27 12:45:04 +0700

    test buttons

--- www/app/Http/Commands/StartCommand.php
+++ www/app/Http/Commands/StartCommand.php
@@ -5,14 +5,19 @@ namespace App\Http\Commands;
 use Telegram\Bot\Commands\Command;
 use Telegram\Bot\Keyboard\Button;
 use Telegram\Bot\Keyboard\Keyboard;
+use Telegram\Bot\Methods\Update;
 
 class StartCommand extends Command
 {
+    use Update;
+
     protected string $name = 'start';
     protected string $description = 'Запуск / Перезапуск бота';
 
     public function handle(): void
     {
+//        $this->getUpdates();
+
         $button1 = Button::make(['text' => 'Найти жилье в аренду', 'url' => 'start.ru']);
         $button2 = Button::make(['text' => 'Купить жильё', 'url' => 'vk.com']);
         $button3 = Button::make(['text' => 'Разместить объект недвижимости для аренды или продажи', 'url' => 'vk.com']);
