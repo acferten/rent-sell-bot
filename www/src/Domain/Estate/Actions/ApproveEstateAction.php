006f60358db2800d342f14daa04475c262bb3077
acferten <gowiththeirname@gmail.com>
2023-11-09 19:43:54 +0700

    fix - cant approve estate twice, steps fix

--- www/src/Domain/Estate/Actions/ApproveEstateAction.php
+++ www/src/Domain/Estate/Actions/ApproveEstateAction.php
@@ -10,13 +10,17 @@ class ApproveEstateAction
 {
     public function __invoke(Nutgram $bot, int $estate_id): void
     {
-        $estate = Estate::where('id', $estate_id)->first();
+        $estate = Estate::find($estate_id);
+
+        if ($estate->status != EstateStatus::closed->value && $estate->status != EstateStatus::pending->value) {
+            $bot->answerCallbackQuery(text: 'Объявление уже имеет статус "Активно"');
+            return;
+        }
 
         $estate->update(['status' => EstateStatus::active]);
 
         $bot->sendMessage('Поздравляем!<b>
 Вы успешно разместили ваш объект. Теперь все соискатели жилья видят его.
 Потенциальные клиенты будут писать вам напрямую.</b>', $estate->user_id, parse_mode: 'html');
-        $bot->deleteMessage('-1001875753187', $bot->callbackQuery()->message->message_id);
     }
 }
