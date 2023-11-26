6c9ca33e2f235a66b6358fc1e8c82135e6d638fd
acferten <gowiththeirname@gmail.com>
2023-11-26 13:08:25 +0700

    welcome message

--- www/routes/web.php
+++ www/routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\EstateViewsController;
 use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return 'Hello World!! This is GetKeysBot Site 🤠';
+});
+
 Route::get('estates/filters', [EstateFiltersController::class, 'get']);
 
 Route::resource('estates', EstateViewsController::class)
